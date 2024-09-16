fab70eccb94f86fdb690a1772a6dea3982e083a6
engin.marshall <engin.marshall@gmail.com>
2024-09-16 19:09:54 +0200

    Init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,7 +33,7 @@ const Home: React.FC = () => {
     <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
+          <IonTitle>Inbox for Engin Marshall</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
